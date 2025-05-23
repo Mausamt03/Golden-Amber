75b00bf2317df778dca0adae6c905be57a0b33fd
Mausamt03 <mausamtripathi52@gmail.com>
2025-05-23 13:08:54 +0530

    Initial commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+  <footer>
+    <div className="container">
+        <div className="banner">
+            <div className="left">
+                GOLDEN AMBER
+            </div>
+            <div className="right">
+                <p> SHANTIPURAM Prayagraj , Utter Pradesh , INDIA </p>
+                <p>Open :</p>10:00 AM - 10:00 PM <p/>
+            </div>
+        </div>
+        <div className="banner">
+            <div className="left">
+                <p>Developed By MAUSAM</p>
+            </div>
+            <div className="right">
+                <p>All Rights Reserved By Mausam.</p>
+            </div>
+        </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer;
